583ecd00d74dad98be010043a44d81f742b851dd
Muhamad Ridwan <versona0450@gmail.com>
2025-05-31 21:26:44 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='flex p-4 h-screen bg-gray-800 text-white'>
+        <div>
+        <input type="text" className='input border' />
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
